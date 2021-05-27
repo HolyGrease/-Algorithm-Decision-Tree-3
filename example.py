1291dac0dae6739e04ca737f4692478ade3fc1af
Danil <kireevdanil2000@gmail.com>
2021-05-27 21:06:01 +0300

    fill example

--- example.py
+++ example.py
@@ -3,17 +3,34 @@ from tree import make_tree
 from tree import Root
 
 def main():
-	tennis = Dataset.get_tennis()
+	iris = Dataset.get_iris()
 
-	tennis.print(10)
+	for i in range(4):
+		iris.threshold(i, "gain")
 
-	tennis = tennis.shuffle()
+	iris.print(10)
 
-	train, test = tennis.split_by_ratio(0.5)
+	iris = iris.shuffle()
+
+	train, test = iris.split_by_ratio(0.5)
 
 	tree = make_tree(train)
 
-	tree.print("")
+	correct = 0
+	for row in test.data:
+		# Get correct value
+		assert_value = row.pop(test.target)
+		# Make prediction
+		predicted_class = tree.classify(row)
+		# If prediction is correct
+		if predicted_class == assert_value:
+			correct += 1
+		if predicted_class is None:
+			predicted_class = "None"
+		# Print compare log to termainal
+		print("{:<15} ?= {:<15}".format(assert_value, predicted_class))
+	# Count and print accuracy
+	print("Acurracy: {:1.2}".format(correct / test.get_rows_number()))
 
 if __name__ == '__main__':
 	main()
