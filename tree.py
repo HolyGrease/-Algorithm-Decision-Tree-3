61e5e2b1a92ee3f5d87024e19abbd7dee129785e
Danil <kireevdanil2000@gmail.com>
2021-05-29 18:16:38 +0300

    add doc

--- tree.py
+++ tree.py
@@ -1,36 +1,82 @@
 from dataset import Dataset
 
 class Node:
+	"""Node of tree abstract class"""
+	# Indent using to print tree in terminal
 	indent = "\t"
 
 class Root(Node):
+	"""Root is first node of the tree (head)"""
 	def __init__(self, nodes, attribute_index, attribute_name):
+		"""Method create Dataset item
+
+		Args:
+			nodes (list): list of child nodes
+			attribute_index (int): index of column in dataset by which splitted child nodes
+			attribute_name (string): name of column in dataset by which splitted child nodes
+		"""
 		self.nodes = nodes
 		self.attribute_index = attribute_index
 		self.attribute_name = attribute_name
 
 	def print(self, prefix=""):
+		"""Method print tree to terminal
+
+		Args:
+			prefix (string): indent used for this node and children nodes
+		"""
+		# Print Root's attribute name
 		print(prefix + self.attribute_name)
+		# Print child nodes
 		for node in self.nodes:
 			node.print(prefix + self.indent)
 
 	def classify(self, row):
-		row = row.copy()
-		attribute_value = row.pop(self.attribute_index)
-		for node in self.nodes:
-			if node.attribute_value == attribute_value:
-				return node.classify(row)
+		"""Method classify instance
 
+		Args:
+			row (list): instance to classify
+
+		Returns:
+			class: prediction for this instance
+			None: if can't classify
+		"""
+		# Get copy of the insance
+		# to not modify origin one
+		row = row.copy()
+		# Get attribute value
+		attribute_value = row.pop(self.attribute_index)
+		# Search in each child node
+		for node in self.nodes:
+			# If attribute value same as in child node
+			if node.attribute_value == attribute_value:
+				# As child node to classify
+				return node.classify(row)
+		# If no childs with same value
 		return None
 
 class Branch(Node):
+	"""Branch is a node of the tree that fork tree"""
 	def __init__(self, nodes, attribute_index, attribute_name, attribute_value):
+		"""Method create Dataset item
+
+		Args:
+			nodes (list): list of child nodes
+			attribute_index (int): index of column in dataset by which splitted child nodes
+			attribute_name (string): name of column in dataset by which splitted child nodes
+			attribute_value: value of attribute that lead to this node
+		"""
 		self.nodes = nodes
 		self.attribute_index = attribute_index
 		self.attribute_name = attribute_name
 		self.attribute_value = attribute_value
 
 	def print(self, prefix):
+		"""Method print tree to terminal
+
+		Args:
+			prefix (string): indent used for this node and children nodes
+		"""
 		print(prefix, end=" ")
 		print(self.attribute_value, end=" -> ")
 		print(self.attribute_name)
@@ -38,45 +84,91 @@ class Branch(Node):
 			node.print(prefix + self.indent)
 
 	def classify(self, row):
-		attribute_value = row.pop(self.attribute_index)
-		for node in self.nodes:
-			if node.attribute_value == attribute_value:
-				return node.classify(row)
+		"""Method classify instance
 
+		Args:
+			row (list): instance to classify
+
+		Returns:
+			class: prediction for this instance
+			None: if can't classify
+		"""
+		# Get attribute value
+		attribute_value = row.pop(self.attribute_index)
+		# Search in each child node
+		for node in self.nodes:
+			# If attribute value same as in child node
+			if node.attribute_value == attribute_value:
+				# As child node to classify
+				return node.classify(row)
+		# If no childs with same value
 		return None
 
 class Leaf(Node):
+	"""Leaf is a node of tree that ends this Branch"""
 	def __init__(self, attribute_value, target_value):
+		"""Method create Dataset item
+
+		Args:
+			attribute_value: value of attribute that lead to this node
+			target_value: value of node
+		"""
 		self.attribute_value = attribute_value
 		self.target_value = target_value
 
 	def print(self, prefix):
+		"""Method print tree to terminal
+
+		Args:
+			prefix (string): indent used for this node
+		"""
+		# Print prefix
 		print(prefix, end=" ")
+		# Print attribute value that lead to this node
 		print(self.attribute_value, end=" -> ")
+		# Print target value
 		print(f"[{self.target_value}]")
 
 	def classify(self, row):
+		"""Method classify instance
+
+		Args:
+			row (list): instance to classify
+
+		Returns:
+			class: prediction for this instance
+		"""
+		# Return prediction
 		return self.target_value
 
-"""
-Outlook
-	Sunny -> Humidity
-		High -> No
-		Normal -> Yes
-	Overcast -> Yes
-	Wind
-		Strong -> No
-		Weak -> Yes
-"""
-
+# TODO fit the tree
 def make_tree(dataset):
+	"""Methods create tree based on passed dataset
+
+	Args:
+		dataset (Dataset): dataset to create tree
+
+	Returns:
+		tree (Root): tree for classification
+		tree (Leaf): if dataset contains only one class
+	"""
 	return make_tree_helper(dataset)
 
-# TODO fit the tree
 def make_tree_helper(dataset, attribute_value=-1):
+	"""Method create tree based on passed dataset
+
+	Args:
+		dataset (Dataset): dataset to create tree
+		attribyte_value: value that leads to this part of tree
+			-1 if no tree before (in this case create Root)
+
+	Returns:
+		tree (Root): tree for classification
+		tree (Leaf): if dataset contains only one class
+	"""
 	# If dataset contains only one class
 	if Dataset.entropy(dataset.get_target_column()) == 0:
-		#
+		# Create and return leaf
 		return Leaf(attribute_value, dataset.get_target_column()[0])
 	# If dataset contains only one column (target column)
 	if dataset.get_columns_number() == 1:
